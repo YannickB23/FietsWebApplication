80feb00eb602d7bea236d66f534d8b98f9d1302f
YANNICK BETERAMS <YANNICK.BETERAMS@student.intecbrussel.be>
2021-02-03 15:40:30 +0100

    Start with database

---
+++ FietsWebApplication/Controllers/BikeController.cs
@@ -0,0 +1,29 @@
+﻿using FietsWebApplication.Interfaces;
+using FietsWebApplication.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FietsWebApplication.Controllers
+{
+    public class BikeController : Controller
+    {
+        private readonly IBikeRepository _bikeRepository;
+        private readonly ICategoryRepository _categoryRepository;
+        public BikeController(IBikeRepository bikeRepository, ICategoryRepository categoryRepository)
+        {
+            _bikeRepository = bikeRepository;
+            _categoryRepository = categoryRepository;
+        }
+        public ViewResult List()
+        {
+            BikesListViewModel bikesListViewModel = new BikesListViewModel();
+            bikesListViewModel.Bikes = _bikeRepository.AllBikes;
+
+            bikesListViewModel.CurrentCategory = "Race Fietsen";
+            return View(bikesListViewModel);
+        }
+    }
+}
---
+++ FietsWebApplication/ViewModels/BikesListViewModel.cs
@@ -0,0 +1,14 @@
+﻿using FietsWebApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FietsWebApplication.ViewModels
+{
+    public class BikesListViewModel
+    {
+        public IEnumerable<Bike> Bikes { get; set; }
+        public string CurrentCategory { get; set; }
+    }
+}
